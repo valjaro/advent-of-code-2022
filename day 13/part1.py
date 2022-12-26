c53aab4e71a7d11a9f438d6a9ae0b9264ceede54
Victor Aljaro <49449038+valjaro@users.noreply.github.com>
2022-12-26 11:45:27 +0100

    day 13 part 2

--- day 13/part1.py
+++ day 13/part1.py
@@ -1,8 +1,3 @@
-# input = [line.strip() for line in open('day 13\input.txt').read()]
-
-# for i in input:
-#     print(i)
-
 with open('day 13\input.txt') as f:
     input = f.read().split("\n\n")
 wrong = 0
@@ -22,10 +17,12 @@ def compare(l, r):
         return 0
     if l < r:
         return 1
-    if r > l:
+    if r < l:
         return -1
 
 s = 0
+
+pkts = []
 for i in range(len(pairs)):
     pair = pairs[i]
     l, r = pair.split("\n")
@@ -33,22 +30,21 @@ for i in range(len(pairs)):
         raise Exception("no")
     l = eval(l)
     r = eval(r)
+    pkts.append(l)
+    pkts.append(r)
     if compare(l, r) == 1:
         s += i + 1
 print(s)
-pkts = []
-# for i in range(len(pairs)):
-#     pair = pairs[i]
-#     l, r = pair.split("\n")
-#     pkts.append(l)
-#     pkts.append(r)
 
-
+# PART 2
 pkts.append([[2]])
 pkts.append([[6]])
 
 for i in range(len(pkts)):
     for j in range(len(pkts) - 1):
-        if compare(pkts[j], pkts[j+1]) > 0:
+        if compare(pkts[j], pkts[j+1]) < 0:
             pkts[j], pkts[j + 1] = pkts[j + 1], pkts[j]
-r = [i for i in range(len(pkts)) if pkts[i] == [[2]] or pkts[i] == [[6]]]
+result = [i+1 for i in range(len(pkts)) if pkts[i] == [[2]] or pkts[i] == [[6]]]
+from functools import reduce
+result = reduce((lambda x, y: x * y), result)
+print(result)
